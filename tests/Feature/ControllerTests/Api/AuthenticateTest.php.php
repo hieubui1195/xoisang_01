b8f0a8973e6959df008b61a3e0c4f881674a722d
buihi <hieubui1195@gmail.com>
2018-04-18 09:33:55 +0700

    build structure / config, view

---
+++ tests/Feature/ControllerTests/Api/AuthenticateTest.php.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\ControllerTests\Api;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AuthenticateTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
